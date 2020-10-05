6391fd88ceb794cacaf2754f9c8a224d75cc1733
Chuarchenaguer <carliski@gmail.com>
2020-10-06 01:10:01 +0200

    AE1-2

---
+++ PMM/AE1/AE1-2.js
@@ -0,0 +1,11 @@
+var meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+//a
+document.write("hola");
+for (var i=0; i<=11; i++) {
+    document.write(meses[i] + "<br>");
+}
+document.write("<br>"+"<br>")
+//b
+for (var i=0; i<=11; i++) {
+    document.write(meses[i] + " ");
+}
