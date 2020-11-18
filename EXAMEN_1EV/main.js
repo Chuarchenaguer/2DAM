4fa5a80e9fb026df98b28938d88a6dbe0ed44132
Chuarchenaguer <carliski@gmail.com>
2020-11-18 17:12:42 +0100

    EXAMEN_1EV commit vacio

--- EXAMEN_1EV/main.js
+++ EXAMEN_1EV/main.js
@@ -2,12 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const texto = document.querySelector('text-container');
     let letra = document.querySelector('input-container');
 
-    let index = texto.indexOf(letra);
+    texto.style.display = 'none';
+    
+    /*let index = texto.indexOf(letra);
 
     if(index >=0){
         console.log("hola");
     } else {
         console.log("no hola")
-    }
-
+    }*/
 });
