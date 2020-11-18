addde29222ffcd059898cd4943d560368bcb4aa5
Chuarchenaguer <carliski@gmail.com>
2020-11-18 14:27:58 +0100

    funcionalidades añadidas

--- main.js
+++ main.js
@@ -23,45 +23,49 @@ document.addEventListener('DOMContentLoaded', () => {
     parrafo.style.textAlign = 'center';
     h1.style.font = '2em sans-serif';
     parrafo.style.font = '1em sans-serif';
-
-});
-
-
-/*    button.addEventListener('botonAdelante'.'click', () => {
-    const step1Container = document.getElementById('step-1');
-    const step2Container = document.getElementById('step-2');
-    const step3Container = document.getElementById('step-3');
-    const step4Container = document.getElementById('step-4');
-    
-    /*if (index === 1){
+    let button1 = document.getElementById("1");
+    let button0 = document.getElementById("0");
+    function Continuar(){
         if (step1Container.style.display === '') {
             step1Container.style.display = 'none';
             step2Container.style.display = '';
-        } else if (step2Container === '' ) {
+        } else if (step2Container.style.display === '' ) {
             step2Container.style.display = 'none';
             step3Container.style.display = '';
-        } else if (step3Container === '') {
+        } else if (step3Container.style.display === '') {
             step3Container.style.display = 'none';
             step4Container.style.display = '';
         }
-    }*/
-/*}));*/
-
-    /*document.addEventListener('botonAtras'.click, () => {
-        const botonAtras = document.getElementsByClassName('botonAtras');
-        const step1Container = document.getElementById('step-1');
-        const step2Container = document.getElementById('step-2');
-        const step3Container = document.getElementById('step-3');
-        const step4Container = document.getElementById('step-4');
-    
+    }
+    function Atras(){
         if (step4Container.style.display === '') {
             step3Container.style.display = '';
             step4Container.style.display = 'none';
-        } else if (step2Container === '' ) {
+        } else if (step2Container.style.display === '' ) {
             step2Container.style.display = 'none';
             step1Container.style.display = '';
-        } else if (step3Container === '') {
+        } else if (step3Container.style.display === '') {
             step3Container.style.display = 'none';
             step2Container.style.display = '';
         }
-});*/
+    }
+    button1.onclick = Continuar;
+    button0.onclick = Atras;
+    //con document.querySelector podemos acceder a etiquetas o clases >> document.querySelector('div')  document.querySelector('step-container') o id: document.querySelector('#step-1')
+
+    const formulario = document.getElementById('formulario');
+
+    formulario.addEventListener('submit', copiarDatos);
+
+    function copiarDatos(event) {
+    event.preventDefault();
+    const nombre = document.querySelector('input[name=nombre]').value;
+    const nacido = document.querySelector('input[name=nacido]').value;
+    const inputsSexo = Array.from(document.querySelectorAll('input[name=hm]'));
+    const sexo = inputsSexo.find(input => input.checked).value;
+    
+    document.getElementById('nombre').innerHTML='Nombre: ' + nombre;
+    document.getElementById('nacido').innerHTML=' Año de nacimiento' + nacido;
+    document.getElementById('sexo').innerHTML= 'Sexo: '+ (sexo == 'h') ? 'Hombre' : 'Mujer';
+    }
+});
