ff15246c3db58d4fcf541e8b63bdf93530f03ec2
Chuarchenaguer <carliski@gmail.com>
2020-11-18 18:16:42 +0100

    EXAMEN_1EV vacío

--- EXAMEN_1EV/main.js
+++ EXAMEN_1EV/main.js
@@ -1,14 +1,2 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const texto = document.querySelector('text-container');
-    let letra = document.querySelector('input-container');
-
-    texto.style.display = 'none';
-    
-    /*let index = texto.indexOf(letra);
-
-    if(index >=0){
-        console.log("hola");
-    } else {
-        console.log("no hola")
-    }*/
 });
