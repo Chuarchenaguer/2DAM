ff96478582b5bfa54e97b457fef0144cf759a3dc
Chuarchenaguer <carliski@gmail.com>
2020-11-18 20:51:20 +0100

    EXAMEN_1EV Ejercicio4 completado

--- EXAMEN_1EV/main.js
+++ EXAMEN_1EV/main.js
@@ -1,2 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const cuerpo = document.querySelector('.text-container');
+    //texto = cuerpo.innerHTML;
+    //console.log(texto);
+
+    cuerpo.addEventListener('mouseover', function(){
+        cuerpo.style.fontWeight = 'bold';
+        cuerpo.style.backgroundColor = 'silver';
+    })
+    cuerpo.addEventListener('mouseout', function(){
+        cuerpo.style.fontWeight = 'normal';
+        cuerpo.style.backgroundColor = 'white';
+    })
 });
